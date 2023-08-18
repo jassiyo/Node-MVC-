f15a0e159367201320fdb44522814f9576543153
Jaskirat <jaskirathsingh2@gmail.com>
2023-08-19 01:20:44 +0530

    Created sign in and sign up page

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -5,3 +5,26 @@ module.exports.profile = function(req,res){
     })
     // res.end('<h1> User profile </h1>')
 }
+
+
+// render the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign-up', { 
+        title: 'Codeial | Sign Up'
+})
+} 
+// reder the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign-in',{
+        title: 'Codeial | Sign In'
+    })
+}
+
+// Get data for sign up page 
+module.exports.create = function(req,res){
+// TODO later
+}
+//Sign in and creating session for user
+module.exports.createSession = function(req, res){
+// TODO later
+};
--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,7 @@ const userController = require('../controller/user_controller')
 
 
 router.get('/profile', userController.profile)
+router.get('/sign-up', userController.signUp)
+router.get('/sign-in', userController.signIn)
+
 module.exports = router
